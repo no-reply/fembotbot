ff1ff38c16bdf33464062ecf339e8f9e5b2d6307
Tom Johnson <thomas.johnson@oregonstate.edu>
2013-07-19 13:54:02 -0700

    better hashtag, response configuration

--- fembot.rb
+++ fembot.rb
@@ -2,14 +2,22 @@ require 'chatterbot/dsl'
 require 'yaml'
 
 conf = YAML.load_file('config.yml')
+tags = {}
 
-for hashtag in conf['hashtags']
-  puts hashtag
-  # search("'surely you must be joking'") do |tweet|
-  #   reply "@#{tweet_user(tweet)} I am serious, and don't call me Shirley!", tweet
+for response in conf['responses'].keys
+  for hashtag in conf['responses'][response]
+    tags[hashtag] = [] unless tags.include?(hashtag)
+    tags[hashtag] << response
+  end
+end
+
+for hashtag in tags.keys
+  puts "#{hashtag}:  #{tags[hashtag][0]}"
+  # search(hashtag) do |tweet|
+  #   reply "@#{tweet_user(tweet)} #{tags[hashtag][0]}", tweet
   # end
 end
 
 replies do |tweet|
-   reply "#USER# I'm a feminist robot!", tweet
+  reply "#USER# I'm a feminist robot!", tweet
 end
