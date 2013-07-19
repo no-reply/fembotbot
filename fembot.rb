d5160a670a26d766085f46e0f6dd709346910858
Tom Johnson <thomas.johnson@oregonstate.edu>
2013-07-19 16:22:57 -0700

    this is *kind of* a working bot, now

--- fembot.rb
+++ fembot.rb
@@ -1,8 +1,17 @@
+
 require 'chatterbot/dsl'
+require 'redis'
 require 'yaml'
+require 'json'
 
 conf = YAML.load_file('config.yml')
 tags = {}
+
+redis = Redis.new()
+
+blklist = []
+blklist = JSON.parse(redis.get('blacklist')) unless redis.get('blacklist').nil?
+blacklist blklist
 
 for response in conf['responses'].keys
   for hashtag in conf['responses'][response]
@@ -11,20 +20,39 @@ for response in conf['responses'].keys
   end
 end
 
+# create a since_id if none exists.
+# this keeps us from resending old tweets
+# if the bot goes up on another system
+if redis.get('since').nil?
+  # searching #fembot is a hurried hack
+  search('#fembot') do |tweet| 
+    redis.set 'since', tweet.id
+  end
+end
+
+since_id = redis.get('since') 
+
 loop do
   begin
     for hashtag in tags.keys
-      puts "#{hashtag}:  #{tags[hashtag][0]}"
-      # search(hashtag) do |tweet|
-      #   reply "@#{tweet_user(tweet)} #{tags[hashtag][0]}", tweet
-      # end
+      search(hashtag) do |tweet|
+        redis.set tweet.from_user, [hashtag].to_json if redis.get(tweet.from_user).nil?
+        reply "@#{tweet_user(tweet)} #{tags[hashtag][0]}", tweet unless JSON.parse(redis.get(tweet.from_user)).include?(hashtag)
+        redis.set 'since', tweet.id
+        user_tags = JSON.parse(redis.get(tweet.from_user)) << hashtag
+        redis.set tweet.from_user, user_tags.to_json
+        redis.incr(hashtag)
+      end
     end
 
     replies do |tweet|
+      if tweet.text.downcase.include?(conf['blacklist_text'])
+        blklist << tweet.from_user
+        redis.set 'blacklist', blklist.to_json
+        blacklist blklist
+      end
       reply "#USER# I'm a feminist robot!", tweet
-      puts 'replied'
     end
-
     sleep 90
   rescue Twitter::Error::TooManyRequests
     sleep 1000
